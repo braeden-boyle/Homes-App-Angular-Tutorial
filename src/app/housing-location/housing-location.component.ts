f1ebe79321523ef91594dc4367223b5cc8d009d9
braeden-boyle <2braedenboyle@gmail.com>
2024-09-20 13:29:10 -0400

    Developed details page and added routing

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,21 +1,22 @@
 import { Component, Input } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HousingLocation } from '../housinglocation';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   template: `
     <section class="listing">
       <img
-        class="listing-photo"
-        [src]="housingLocation.photo"
-        alt="Exterior photo of {{ housingLocation.name }}"
-        crossorigin
+      class="listing-photo"
+      [src]="housingLocation.photo"
+      alt="Exterior photo of {{ housingLocation.name }}"
+      crossorigin
       />
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">{{ housingLocation.city }}, {{ housingLocation.state }}</p>
+      <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
     </section>
   `,
   styles: `
@@ -46,7 +47,7 @@ import { HousingLocation } from '../housinglocation';
       color: var(--primary-color);
     }
     section.listing a::after {
-      content: "\x83A";
+      // content: "\x83A";
       margin-left: 5px;
     }
   `
